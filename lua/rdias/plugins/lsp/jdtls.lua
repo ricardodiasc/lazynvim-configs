6c218433ce2caf0310f11c00c2b15caec67ac1f6
Ricardo Dias <ricardodc@gmail.com>
2023-11-13 03:04:25 -0300

    Initial commit for java dap config

--- lua/rdias/plugins/lsp/jdtls.lua
+++ lua/rdias/plugins/lsp/jdtls.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "mfussenegger/nvim-dap",
     "nvim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter"
   },
   config = function()
     local jdtls = require("jdtls")
@@ -176,12 +177,133 @@ return {
         opts.desc = "Show code actions."
         keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 
-        -- require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+        require('jdtls').setup_dap({ hotcodereplace = 'auto' })
       end
 
       -- Existing server will be reused if the root_dir matches.
       jdtls.start_or_attach(config)
       -- not need to require("jdtls.setup").add_commands(), start automatically adds commands
+      function get_spring_boot_runner(profile, debug)
+        local debug_param = ""
+
+        if (debug) then
+          debug_param = " --debug-jvm "
+        end
+
+        local profile_param = ""
+        if (profile) then
+          profile_param = " --args=\"--spring.profiles.active=" .. profile .. "\" "
+        end
+
+        return "./gradlew bootRun " .. debug_param .. profile_param
+      end
+
+      function run_spring_boot(debug)
+        -- vim.cmd('term ' .. get_spring_boot_runner('local', debug))
+        vim.cmd("TermExec cmd='" .. get_spring_boot_runner('local', debug) .. "' ")
+        -- vim.api.nvim_set_keymap("n", "<cmd>TermExec cmd='" .. get_spring_boot_runner() .. ";exit' <CR>", {noremap = true, silent = true})
+      end
+
+      function attach_to_debug()
+        local dap = require('dap')
+
+        dap.configurations.java = {
+          {
+            type = 'java',
+            request = 'attach',
+            name = "Attach to the process",
+            hostName = 'localhost',
+            port = '5005',
+          }
+        }
+        dap.continue()
+      end
+      local keymap = vim.keymap
+
+      keymap.set('n', '<Leader>da', ':lua attach_to_debug()<CR>')
+
+      keymap.set('n', '<Leader>joi', ':lua require("jdtls").organize_imports()<CR>')
+      keymap.set('n', '<Leader>jc', ':lua require("jdtls").compile("incremental")<CR>')
+      keymap.set('n', '<Leader>jsr', function() run_spring_boot() end)
+
+      keymap.set('n', '<F9>', function() run_spring_boot(false) end)
+      keymap.set('n', '<F10>', function() run_spring_boot(true) end)
+
+
+
+      -- Dap UI Toggle not working yet
+      keymap.set('n', '<Leader>dt', ':DapUiToggle<CR>')
+      keymap.set('n', '<F5>', ':lua require"dap".continue()<CR>')
+      keymap.set('n', '<F6>', ':lua require"dap".step_over()<CR>')
+      keymap.set('n', '<F7>', ':lua require"dap".step_into()<CR>')
+      keymap.set('n', '<F8>', ':lua require"dap".step_out()<CR>')
+
+      keymap.set('n', '<Leader>b', ':lua require"dap".toggle_breakpoint()<CR>')
+      keymap.set('n', '<Leader>B', ':lua require"dap".set_breakpoint(vim.fn.input("Condition: "))<CR>')
+      keymap.set('n', '<Leader>dl', ':lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log: "))<CR>')
+      keymap.set('n', '<Leader>dr', ':lua require"dap".repl.open()<CR>')
+
+
+
+      local utils = require('rdias/utils')
+
+
+      function get_test_runner(test_name, debug)
+        if debug then
+          return './gradlew test --debug-jvm --tests "' .. test_name .. '"'
+        end
+
+        return './gradlew test --tests "' .. test_name .. '"'
+      end
+
+      function run_java_test_method(debug)
+        -- local utils = require('rdias/utils')
+        local method_name = utils.get_current_full_method_name()
+        -- print(get_test_runner(method_name, false))
+        -- vim.cmd('term ' .. get_test_runner(method_name, debug) )
+
+        vim.cmd("TermExec cmd='" .. get_test_runner(method_name, debug) .. "' ")
+      end
+
+      local function run_java_test_class(debug)
+        -- local utils = require 'user/utils'
+        local class_name = utils.get_current_full_class_name()
+        -- vim.cmd('term ' .. get_test_runner(class_name, debug))
+
+        vim.cmd("TermExec cmd='" .. get_test_runner(class_name, debug) .. "' ")
+      end
+
+      keymap.set('n', '<Leader>jtm', function() run_java_test_method(false) end)
+      keymap.set('n', '<Leader>jTM', function() run_java_test_method(true) end)
+      keymap.set('n', '<Leader>jtc', function() run_java_test_class(false) end)
+      keymap.set('n', '<Leader>jTC', function() run_java_test_class(true) end)
+
+      -- Nvtree
+      keymap.set('n', '<Leader>e', '<cmd>NvimTreeFindFileToggle<CR>')
+
+
+      -- Git
+      keymap.set('n', '<Leader>gb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+
+
+      -- Trouble
+      keymap.set('n', '<Leader>xt', '<cmd>TroubleToggle<CR>')
+      keymap.set('n', '<Leader>xd', '<cmd>TroubleToggle document_diagnostics<CR>')
+      keymap.set('n', '<Leader>xq', '<cmd>TroubleToggle quickfix<CR>')
+      keymap.set('n', 'gR', '<cmd>TroubleToggle lsp_references<CR>')
+
+      function show_dap_centered_scopes()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.scopes)
+      end
+
+      function show_dap_repl()
+        local repl = require('dap').repl
+        repl.open()
+      end
+
+      keymap.set('n', 'gs', ':lua show_dap_centered_scopes()<CR>')
+      keymap.set('n', 'gR', ':lua show_dap_repl()<CR>')
     end
 
     attach_jdtls()
