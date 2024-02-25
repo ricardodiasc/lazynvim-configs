a31d65b6e7293fdc18f23d5b8b79b015bfc19699
Ricardo Dias <ricardodc@gmail.com>
2024-02-25 01:38:01 -0300

    Fix Harpoon keybindings

--- lua/rdias/plugins/harpoon.lua
+++ lua/rdias/plugins/harpoon.lua
@@ -7,9 +7,9 @@ return {
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>hh", "<CMD>lua require('harpoon.ui').toggle_quick_menu()<cr>")
-    keymap.set("n", "<leader>hm", "<CMD>lua require('harpoon.mark').add_file()")
-    keymap.set("n", "<leader>hn", "<CMD>lua require('harpoon.ui').nav_next()")
-    keymap.set("n", "<leader>hp", "<CMD>lua require('harpoon.ui').nav_pref()")
-    keymap.set("n", "<leader>h1", "<CMD>lua require('harpoon.ui').nav_file()")
+    keymap.set("n", "<leader>hm", "<CMD>lua require('harpoon.mark').add_file()<cr>")
+    keymap.set("n", "<leader>hn", "<CMD>lua require('harpoon.ui').nav_next()<cr>")
+    keymap.set("n", "<leader>hp", "<CMD>lua require('harpoon.ui').nav_pref()<cr>")
+    keymap.set("n", "<leader>h1", "<CMD>lua require('harpoon.ui').nav_file()<cr>")
   end,
 }
