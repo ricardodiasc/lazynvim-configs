d935680f57a70f53fe67af71e5daf63b61981306
Ricardo Dias <ricardodc@gmail.com>
2024-03-04 08:32:06 -0300

    Replace vrc to rest.nvim and remove dap ui from auto toggle

--- lua/rdias/plugins/lsp/dap.lua
+++ lua/rdias/plugins/lsp/dap.lua
@@ -76,10 +76,10 @@ return {
 
     dapui.setup({})
 
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-
+    -- dap.listeners.after.event_initialized["dapui_config"] = function()
+    --   dapui.open()
+    -- end
+    --
     dap.listeners.before.event_terminated["dapui_config"] = function()
       dapui.close()
     end
