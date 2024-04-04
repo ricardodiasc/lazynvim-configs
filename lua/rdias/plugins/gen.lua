0208f22d967c6a2aef8a14b258710cf98bc44125
Ricardo Dias <ricardodc@gmail.com>
2024-04-04 11:04:24 -0300

    Update gen.nvim configs

--- lua/rdias/plugins/gen.lua
+++ lua/rdias/plugins/gen.lua
@@ -2,12 +2,15 @@ return {
   "David-Kunz/gen.nvim",
   opts = {
     model = "deepseek-coder:6.7b",
+    quit_map = "q",
     display_mode = 'float',
     show_prompt = false,
     show_model = false,
     no_auto_close = false,
-    command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
-    list_models = "<function>",
+    command = function (opts)
+      local body = { model = opts.model, stream = true }
+      return "curl --silent --no-buffer -X POST http://localhost:11434/api/chat -d $body"
+    end,
     debug = false
   }
 }
