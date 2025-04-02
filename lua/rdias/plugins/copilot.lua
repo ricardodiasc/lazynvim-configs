393b763055676cbb28b2041fadd4c24d5e371069
Ricardo Dias <ricardodc@gmail.com>
2025-04-02 01:13:52 -0300

    Add renderman and copilot new keys

--- lua/rdias/plugins/copilot.lua
+++ lua/rdias/plugins/copilot.lua
@@ -1,3 +1,14 @@
 return {
-  "github/copilot.vim"
+  "github/copilot.vim",
+  config = function ()
+    vim.g.copilot_no_tab_map = true
+
+    local keymap = vim.keymap
+
+    keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', { expr = true, silent = true, replace_keycodes = false, desc = "Accept Copilot suggestion" })
+    keymap.set("i", "<C-K>", 'copilot#Dismiss()', { expr = true, silent = true, replace_keycodes = false, desc = "Dismiss Copilot suggestion" })
+    keymap.set("i", "<C-Right>", 'copilot#Next()', { expr = true, silent = true, replace_keycodes = false, desc = "Next Copilot suggestion" })
+    keymap.set("i", "<C-Left>", 'copilot#Previous()', { expr = true, silent = true, replace_keycodes = false, desc = "Previous Copilot suggestion" })
+  end
+
 }
