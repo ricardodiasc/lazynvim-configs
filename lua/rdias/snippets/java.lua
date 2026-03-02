afef55843ffd68d43d49dcdead9a0dfc79fc95c7
Ricardo Dias <ricardodc@gmail.com>
2026-03-02 01:22:49 -0300

    Update class declaration

--- lua/rdias/snippets/java.lua
+++ lua/rdias/snippets/java.lua
@@ -8,8 +8,30 @@ local function get_filename()
   return vim.fn.expand("%:t:r")
 end
 
+local function get_package_name()
+  local path = vim.fn.expand("%:p:h")
+  local package = path:match("src/main/java/(.+)")
+      or path:match("src/test/java/(.+)")
+      or path:match("java/(.+)")
+      or path:match("src/(.+)")
+  
+  if package then
+    return "package " .. package:gsub("/", ".") .. ";"
+  end
+  return ""
+end
+
+local function get_package_and_header()
+  local pkg = get_package_name()
+  if pkg ~= "" then
+    return { pkg, "", "" }
+  end
+  return ""
+end
+
 return {
   s("class", {
+    f(get_package_and_header, {}),
     t("public class "),
     f(get_filename, {}),
     t(" {"),
@@ -18,6 +40,7 @@ return {
     t({"", "}"}),
   }),
   s("Class", {
+    f(get_package_and_header, {}),
     t("public class "),
     f(get_filename, {}),
     t(" {"),
@@ -26,6 +49,7 @@ return {
     t({"", "}"}),
   }),
   s("interface", {
+    f(get_package_and_header, {}),
     t("public interface "),
     f(get_filename, {}),
     t(" {"),
@@ -34,6 +58,7 @@ return {
     t({"", "}"}),
   }),
   s("enum", {
+    f(get_package_and_header, {}),
     t("public enum "),
     f(get_filename, {}),
     t(" {"),
